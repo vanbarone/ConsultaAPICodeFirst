029b739ae03bec42c142e6b43fc540e63302146b
Vanessa Barone <vanbarone.milani@gmail.com>
2022-09-13 15:26:58 -0300

    Inclusão das referências e da classe 'Context'

---
+++ Context/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ConsultaAPICodeFirst.Context
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+    }
+}
